7c61eaa4b0834af6e3c66623344b908416d9afe5
trytheworld <trytheworld@DESKTOP-VQ2VT8Q>
2018-06-04 17:57:07 +0800

    testHtmlUnit解决抓取动态加载获取数据的问题，需要注意的时在实际使用中等待时间参考HtmlUnit官方给的方法来计算等待时间，不应该写常量

--- src/main/java/com/zys/reptile.java
+++ src/main/java/com/zys/reptile.java
@@ -1,5 +1,14 @@
 package com.zys;
 
+import com.gargoylesoftware.htmlunit.AjaxController;
+import com.gargoylesoftware.htmlunit.BrowserVersion;
+import com.gargoylesoftware.htmlunit.NicelyResynchronizingAjaxController;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.DomElement;
+import com.gargoylesoftware.htmlunit.html.HtmlElement;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.html.HtmlSpan;
+import com.gargoylesoftware.htmlunit.javascript.host.html.HTMLSpanElement;
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.HttpMethod;
 import org.apache.commons.httpclient.methods.GetMethod;
@@ -16,6 +25,10 @@ import java.net.URL;
 
 
 public class reptile {
+    /**
+     * 此方法实验了，jsopu解析脚本路径并且成功转换为绝对路径，
+     * 取js生成的数据，未有思路
+     */
     public void getHtmlResource(){
         Document html =  null;
         HttpClient httpClient = new HttpClient();
@@ -54,8 +67,38 @@ public class reptile {
 //        Document doc = Jsoup.parse(html);
     }
 
+    /**
+     * 实验 htmlunit + jsoup
+     */
+    public void testHtmlUnit()
+    {
+        //构造一个无界面浏览器
+//        WebClient webClient = new WebClient(BrowserVersion.CHROME);//模拟谷歌浏览器
+        WebClient webClient = new WebClient();
+        webClient.getOptions().setCssEnabled(false);//禁用css
+        webClient.getOptions().setThrowExceptionOnScriptError(false);//当脚本出现异常不抛出
+        //webClient.getOptions().setJavaScriptEnabled(false);
+        try {
+            //获得页面
+            HtmlPage page = webClient.getPage("Address");//地址
+            webClient.waitForBackgroundJavaScript(3000);//js后台等待时间
+            webClient.setAjaxController(new NicelyResynchronizingAjaxController());//支持AJAX
+            HtmlSpan span = (HtmlSpan) page.getElementById("displaySpan");
+//            HtmlElement elements = page.getBody();
+            String content = page.querySelector("#price9").getTextContent();
+            System.out.println(content);
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+            if(webClient!=null)webClient.close();
+        }
+
+    }
     public static void main(String[] args) {
         reptile rp = new reptile();
-        rp.getHtmlResource();
+        rp.testHtmlUnit();
     }
 }
