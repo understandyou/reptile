c4f6a885d846af68eb2b2673d72e3d5b2e156b51
trytheworld <trytheworld@DESKTOP-VQ2VT8Q>
2018-06-06 14:09:35 +0800

    selenium异常未解决

--- src/main/java/com/zys/reptile.java
+++ src/main/java/com/zys/reptile.java
@@ -16,15 +16,26 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import us.codecraft.webmagic.Page;
+import us.codecraft.webmagic.Site;
+import us.codecraft.webmagic.Spider;
+import us.codecraft.webmagic.processor.PageProcessor;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.util.Random;
+import java.util.Scanner;
 
 
-public class reptile {
+public class reptile implements PageProcessor {
     /**
      * 此方法实验了，jsopu解析脚本路径并且成功转换为绝对路径，
      * 取js生成的数据，未有思路
@@ -97,8 +108,60 @@ public class reptile {
         }
 
     }
+
+    /**
+     *  模拟用户在浏览器操作，测试工具
+     */
+    public void testSelenium(){
+        //由于火狐安装路径不在默认的c盘，所以需要设置路径
+        //System.setProperty("webdriver.firefox.bin", "C:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe");
+        //System.setProperty("webdriver.gecko.driver", "E:\\st\\reptile\\geckodriver-v0.20.1-win32.zip");
+        WebDriver webDriver = new FirefoxDriver();
+        //最大化
+        webDriver.manage().window().maximize();
+        //访问百度
+        webDriver.get("https://www.baidu.com");
+        //根据页面元素 xpath ，右键元素可获取//*[@id="kw"],这是百度的输入框
+        WebElement account = webDriver.findElement(By.xpath("//*[@id=\"kw\"]"));
+//        actions.sendKeys("selenium java怎么实现方法调用");
+        //根据id获取元素 su ，百度一下的按钮
+        account = webDriver.findElement(By.id("trainPay"));
+        //点击
+        account.click();
+        //获取页面加载的所有文本内容
+        String text = webDriver.findElement(By.tagName("body")).getText();
+        //关闭浏览器（这个包括驱动完全退出，会清除内存），close 是只关闭浏览器
+        webDriver.quit();
+
+
+    }
+
     public static void main(String[] args) {
         reptile rp = new reptile();
-        rp.testHtmlUnit();
+        rp.testSelenium();
+
+//        Spider.create(new reptile())
+//                //开始抓
+//                .addUrl("http://localhost:54336/")
+//                //开启1个线程抓取
+//                .thread(1)
+//                //启动爬虫
+//                .run();
     }
+    // 部分一：抓取网站的相关配置，包括编码、抓取间隔、重试次数等
+    private Site site = Site.me().setRetryTimes(3).setSleepTime(1000);
+    // 实现webMagic的PageProcessor接口
+    // process是定制爬虫逻辑的核心接口，在这里编写抽取逻辑
+    @Override
+    public void process(Page page) {
+        page.putField("key1",page.getUrl());
+        page.putField("name", page.getHtml());//.xpath("//h1[@class='entry-title public']/strong/a/text()").toString()
+    }
+
+    @Override
+    public Site getSite() {
+        return site;
+    }
+
+
 }
